a3f0df243baa2a36c9a43394f0e0077ee4311272
assignment <bmwthriad2023@gmail.com>
2026-03-09 22:50:35 +0600

    problem_4a_cf

---
+++ problem_4a_cf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int w;
+    cin >> w;
+    if(w==2){
+        cout<<"No";
+    }
+    else if(w%2==0){
+        cout<<"Yes";
+    } else {
+    cout<<"No";
+    }
+
+    return 0;
+}
